70a9895e517a7dbda16879e92a852b7a71a21da6
Zahra Tabatabaee <Zahra_Tabatabaee@comp.iust.ac.ir>
2023-08-06 12:01:37 +0330

    login ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'vsmmobile': { 'max': '580px' },
+      'smmobile': { 'min': '581px', 'max': '903px' },
+      'mobile': { 'min': '904px', 'max': '991px' },
+      'tablet': { 'min': '992px', 'max': '1023px' },
+      'laptop':   { 'min': '1024px', 'max': '1239px' },
+      'desktop': { 'min': '1240px' },
+    },
     extend: {},
   },
   plugins: [],
