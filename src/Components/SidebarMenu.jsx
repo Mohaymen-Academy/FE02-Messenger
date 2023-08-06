d0e23ea158b0dc4a51061cbe2a06aed142437932
hesam <bhesam2000@yahoo.com>
2023-08-06 11:40:09 +0430

    work on sidebar

--- src/Components/SidebarMenu.jsx
+++ src/Components/SidebarMenu.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SidebarMenu() {
-  return (
-    <img src="images/menuIcon.png" alt="" />
-  )
+    const [open, setopen] = useState(false);
+    return (
+        <>
+            {
+                open ?
+                    <img src="images/menuIcon.png" className={'w-[20px] h-[20px]'} alt="" />
+                    :
+                    <div>
+                        <img src="" alt="" />
+                        MenuCard
+                    </div>
+            }
+        </>
+    )
 }
