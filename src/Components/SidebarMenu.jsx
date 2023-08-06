47b7ee925f3906028c888c64fedb2c8ac30b5e9e
NimaSaei379 <saeinima@yahoo.com>
2023-08-06 13:54:12 +0330

    fix chatlist component bugs

--- src/Components/SidebarMenu.jsx
+++ src/Components/SidebarMenu.jsx
@@ -1,30 +1,11 @@
-import { useEffect, useState, useRef } from 'react';
 import SidebarCard from './SidebarCard.jsx';
 
-export default function SidebarMenu({ profileImage, username }) {
-  const [open, setopen] = useState(true);
-  const divref = useRef(null);
-  function handleOutsideClick(event) {
-    if (divref.current && !divref.current.contains(event.target)) {
-      console.log('zrap');
-      setopen(false);
-    }
-  }
-  useEffect(() => {
-    if (open) {
-      document.addEventListener('mousedown', handleOutsideClick);
-    }
-    return () => document.addEventListener('mousedown', handleOutsideClick);
-  }, [open]);
-
+export default function SidebarMenu({ profileImage, username, open, divref }) {
   return (
     <div className="h-screen w-[190px]">
-      <button className="fixed" onClick={(e) => setopen((prev) => !prev)}>
-        <img src="images/menuIcon.png" className={'h-[20px] w-[20px]'} alt="" />
-      </button>
       <div
         ref={divref}
-        className={`relative top-0 flex h-screen flex-col bg-blue-700 px-5 transition-all duration-1000 ease-in-out ${
+        className={`fixed top-0 flex h-screen flex-col bg-blue-700 px-5 transition-all duration-1000 ease-in-out ${
           open ? 'right-0' : 'right-[-250px]'
         }`}>
         <div className="flex flex-col border-b-2 border-b-gray-400 px-3 pb-3 pt-5">
