bfe887dc6ffdef278e20572723f47157b290dc7a
hesam <bhesam2000@yahoo.com>
2023-08-07 09:05:21 +0430

    before merge

--- src/Components/SidebarMenu.jsx
+++ src/Components/SidebarMenu.jsx
@@ -12,7 +12,7 @@ export default function SidebarMenu({ profileImage, username, open, divref }) {
   const [menu, setmenu] = useState(NUM_SIDEBAR_SETTINGS);
   const Menues = {
     [NUM_SIDEBAR_DEFAULT]: <Default menuSetter={setmenu} />,
-    [NUM_SIDEBAR_SETTINGS]: <Settings />,
+    [NUM_SIDEBAR_SETTINGS]: <Settings />
     //  , [NUM_SIDEBAR_CONTACTS]:
     //  , [NUM_SIDEBAR_CHANNEL]:
     //  , [NUM_SIDEBAR_GROUP]:
@@ -25,9 +25,19 @@ export default function SidebarMenu({ profileImage, username, open, divref }) {
         className={`w-[250px] fixed top-0 flex h-screen flex-col bg-[#1e7889] transition-all duration-1000 ease-in-out ${
           open ? 'right-0' : 'right-[-255px]'
         }`}>
-        <div className="flex flex-col gap-3  pt-5 pb-3 pr-5 pl-[5.75rem] w-fit">
-          <img src={profileImage} className="w-[70px] h-[70px] rounded-full" alt="" />
-          {username}
+        <div className=" w-[100%] h-[170px] flex flex-col gap-6 pt-0 items-center">
+          <div
+            className="flex justify-center m-0 w-[100%] object-cover
+            h-[100%]"
+            style={{ background: `url(${'images/wall.webp'})` }}>
+            <img
+              src={profileImage}
+              className="w-[70px] h-[70px] rounded-full p-1 bg-[#1e7889] relative bottom-[-70px]"
+              alt=""
+            />
+          </div>
+          <p
+          className={''}>{username}</p>
         </div>
         {Menues[menu]}
       </div>
