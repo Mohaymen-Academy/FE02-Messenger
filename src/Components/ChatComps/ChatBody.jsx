32601dc4e455d6a8216949f6825a4d2e4e105a52
NimaSaei379 <saeinima@yahoo.com>
2023-08-08 10:55:01 +0330

    create message and change chatbody

--- src/Components/ChatComps/ChatBody.jsx
+++ src/Components/ChatComps/ChatBody.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import EmojiPicker from 'emoji-picker-react';
 import ChatFooter from './ChatFooter';
+import Message from './Message';
 
 export default function ChatBody() {
   return (
-    <div dir="ltr" className="flex h-[100%] w-full flex-col">
-      <div className="flex h-[80%] w-full flex-col"></div>
-      <div
-      className='mb-2'>
+    <div dir="rtl" className="flex h-[100%] w-full flex-col">
+      <div className="flex h-[80%] w-full flex-col items-center overflow-hidden">
+        <div className="mb-3 w-full overflow-auto px-5 pt-3">
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+        </div>
+      </div>
+      <div className="mb-2 h-16">
         <ChatFooter />
       </div>
     </div>
