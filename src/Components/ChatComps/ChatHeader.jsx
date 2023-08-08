b29581daec3efaf7096d796b2dfe05563616a6b9
hesam <bhesam2000@yahoo.com>
2023-08-08 10:36:45 +0430

    start working on the chatheader

--- src/Components/ChatComps/ChatHeader.jsx
+++ src/Components/ChatComps/ChatHeader.jsx
@@ -1,10 +1,14 @@
 import Avatar from './Avatar.jsx';
-import { UilEllipsisV } from '@iconscout/react-unicons'
-const ChatHeader = () => (
-  <div className=" flex h-[11%] w-full items-center justify-between bg-slate-500 px-1 font-iRANSans">
+import { UilEllipsisV } from '@iconscout/react-unicons';
+const ChatHeader = ({ setActive  }) => (
+  <div
+    onClick={(e) => {
+      setActive((prev) => !prev);
+    }}
+    className=" flex h-[11%] w-full items-center justify-between bg-slate-500 px-1 font-iRANSans">
     <div id="header-account" className="flex h-full items-center gap-2">
       <div className="flex-1 py-4">
-        <Avatar isOnline={false} image={'a'}/>
+        <Avatar isOnline={false} image={'a'} />
       </div>
       <div className="">
         <h3 className="text-lg font-semibold text-color2">یوزر نیم </h3>
@@ -12,7 +16,7 @@ const ChatHeader = () => (
       </div>
     </div>
     <div className="text-text1 w-8 h-8 mx-1 ">
-      <UilEllipsisV/>
+      <UilEllipsisV />
     </div>
   </div>
 );
