fb83fecc3a5e9a85fa3ec37f3ff6664ec314afca
hesam <bhesam2000@yahoo.com>
2023-08-08 18:19:43 +0430

    still working on the composer

--- src/Components/ChatComps/ChatFooter.jsx
+++ src/Components/ChatComps/ChatFooter.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { UilSmile, UilMessage, UilPaperclip } from '@iconscout/react-unicons';
 import EmojiPicker from 'emoji-picker-react';
 
@@ -6,10 +6,47 @@ export default function ChatFooter() {
   // const [text, setText] = React.useState('');
   const [openEmoji, setOpenEmoji] = useState(false);
   const [openAttach, setOpenAttach] = useState(false);
+  const emoji = useState('');
   const textref = useRef(null);
   const divref = useRef(null);
   function handleonInput() {
-    console.log();
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const clonedRange = range.cloneRange();
+    clonedRange.selectNodeContents(divref.current);
+    clonedRange.setEnd(range.endContainer, range.endOffset); // this set the position of the cursor
+    console.log(clonedRange)
+    const caretPosition = clonedRange.toString().length;
+    console.log('here', caretPosition);
+    console.log(clonedRange)
+    textref.current.textcontent = caretPosition;
+  }
+
+  function handleEmojiAdd() {}
+
+  function handleEmojiPicker(e) {
+    const emoji = e.emoji;
+    const careposition = textref.current.textcontent;
+    console.log(careposition);
+    // console.log(Array.from(divref.current.innerText).splice(2, 0, emoji).join(''));
+    divref.current.innerText = insertCharAtIndex(divref.current.innerText, emoji, careposition);
+    console.log(divref.current.innerText);
+    setOpenEmoji(false);
+  }
+  function insertCharAtIndex(originalString, charToAdd, index) {
+    if (index < 0 || index > originalString.length) {
+      throw new Error('Index out of bounds');
+    }
+    if (index == originalString.length) {
+      console.log('sdfewr');
+      return originalString + charToAdd;
+    }
+    console.log(index);
+
+    const leftPart = originalString.substring(0, index);
+    const rightPart = originalString.substring(index);
+
+    return leftPart + charToAdd + rightPart;
   }
   return (
     <div
@@ -33,14 +70,13 @@ export default function ChatFooter() {
                   }}
                 /> */}
       <div
+        style={{ direction: 'rtl' }}
         ref={divref}
+        onInput={handleonInput}
         contentEditable
+        onChange={(e) => console.log()}
         className=" w-[90%] max-h-[50px] outline-none h-auto overflow-hidden shadow-none border-none break-all focus:shadow-none active:shadow-none">
-        <span
-          ref={textref}
-          role="textbox"
-          contentEditable
-          onInput={(e) => (textref.current = e.target.innerText)}></span>
+        <span ref={textref} role="textbox" contentEditable></span>
       </div>
       <div>
         <button onClick={() => setOpenEmoji(!openEmoji)} className="mx-1 h-8 w-8 text-text1 ">
@@ -48,13 +84,7 @@ export default function ChatFooter() {
         </button>
         {openEmoji && (
           <div className="absolute bottom-[60px] left-[20px] z-10">
-            <EmojiPicker
-              theme={localStorage.getItem('theme')}
-              onEmojiClick={(e) => {
-                textref.current = textref.current + e.emoji;
-                setOpenEmoji(false);
-              }}
-            />
+            <EmojiPicker theme={localStorage.getItem('theme')} onEmojiClick={handleEmojiPicker} />
           </div>
         )}
         {/* {openAttach &&
--- src/Components/ProfileEdit.jsx
+++ src/Components/ProfileEdit.jsx
@@ -22,18 +22,18 @@ export default function ProfileEdit() {
             className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900  rounded-lg border-2  border-blue-500 appearance-none  bg-color2 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           />
-          <label class="absolute text-sm text-gray-500 bg-color2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0]   peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+          <label className="absolute text-sm text-gray-500 bg-color2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0]   peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
             نام الزامی
           </label>
         </div>
-        <div class="relative">
+        <div className="relative">
           <input
             type="text"
             id="floating_outlined"
-            class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900  rounded-lg border-2  border-blue-500 appearance-none  bg-color2 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900  rounded-lg border-2  border-blue-500 appearance-none  bg-color2 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
           />
-          <label class="absolute text-sm text-gray-500 bg-color2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0]   peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
+          <label className="absolute text-sm text-gray-500 bg-color2 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0]   peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">
             نام خانوادگی
           </label>
           <div>{<CheckBoxParag title={'اعلان ها'} />}</div>
