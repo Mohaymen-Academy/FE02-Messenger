3868ee238bdb6865b5b318347b2b7d9b9430b53c
hesam <bhesam2000@yahoo.com>
2023-08-12 15:58:42 +0430

    work on the bottom director

--- src/Components/ChatComps/ChatBody.jsx
+++ src/Components/ChatComps/ChatBody.jsx
@@ -6,7 +6,7 @@ import ImagePreviewer from '../media-previewer/ImagePreviewer.jsx';
 import { TYPE_CHANNEL, TYPE_GROUP } from '../../utility/Constants.js';
 import MessageDateGroup from '../message/MessageDateGroup.jsx';
 import MessageVoice from '../message/MessageVoice.jsx';
-
+import { UilArrowDown } from '@iconscout/react-unicons';
 const messages = [
   {
     messageID: 2,
@@ -60,26 +60,51 @@ const messages = [
 
 export default function ChatBody({ chattype }) {
   const bodyref = useRef(null);
-  const footerallowed = chattype == TYPE_CHANNEL ? false : chattype == TYPE_GROUP ? true : true;
-  // Todo FOR USER and Group must be modifed
-  const [preview, setPreview] = React.useState(false);
-  function handleRightClick(event, index) {
-    event.preventDefault();
+  const [buttonhidden, setbuttonhidden] = useState(true);
+  function handleonScroll() {
+    // console.log('zarp',bodyref.current?.scrollTop,bodyref.current.clientHeight)
+    if (bodyref.current?.scrollTop == bodyref.current.scrollHeight - bodyref.current.clientHeight) {
+      console.log('zarp');
+      setbuttonhidden(true);
+    } else {
+      if (buttonhidden) {
+        setbuttonhidden(false);
+      }
+    }
   }
+  const footerallowed = chattype == TYPE_CHANNEL ? false : chattype == TYPE_GROUP ? true : true;
+  const [preview, setPreview] = useState(false);
+  // function handleRightClick(event, index) {
+  //   event.preventDefault();
+  // }
+
   function scrolltobottom() {
     // bodyref.current.scrollTop = bodyref.current.scrollHeight;
-    const scrollHeight = bodyref.current.scrollHeight;
-    const scrollStep = Math.PI / (scrollHeight / 2);
-    let count = 0;
-    function scrollAnimation() {
-      if (bodyref.current.scrollTop + bodyref.current.clientHeight < scrollHeight) {
-        bodyref.current.scrollTop = scrollHeight * 50 * (1 - Math.cos(scrollStep * count));
-        count++;
+    const startPosition = bodyref.current.scrollTop;
+    const endPosition = bodyref.current.scrollHeight - bodyref.current.clientHeight;
+    const duration = 1000;
+    const startTime = performance.now();
+
+    // const scrollHeight = bodyref.current.scrollHeight;
+    // const scrollStep = Math.PI / (scrollHeight / 2);
+    // let count = 0;
+    function scrollAnimation(timestamp) {
+      const elapsed = timestamp - startTime;
+      const progress = Math.min(elapsed / duration, 1);
+      const easedProgress = easeInOutCubic(progress); // Apply easing function if desired
+      const newPosition = startPosition + (endPosition - startPosition) * easedProgress;
+      bodyref.current.scrollTop = newPosition;
+      if (progress < 1) {
         requestAnimationFrame(scrollAnimation);
       }
     }
+    function easeInOutCubic(t) {
+      return t < 0.5 ? 4 * t * t * t : 1 - Math.pow(-2 * t + 2, 3) / 2;
+    }
 
-    scrollAnimation();
+    requestAnimationFrame(scrollAnimation);
+
+    // scrollAnimation();
     // console.log(bodyref.current)
     // bodyref.current.scrollIntoView({ behavior: 'smooth' });
   }
@@ -89,19 +114,28 @@ export default function ChatBody({ chattype }) {
       dir="rtl"
       className={
         `flex h-[100%] flex-col
-      
-      // mb-[-30px]
-      `
+        // mb-[-30px]
+        `
         // mb-[-150px]
       }>
       <div className="flex h-[70%] w-full flex-col items-center overflow-hidden">
         <div
           className="mb-2 h-[105vh] w-full overflow-auto px-5 pt-3"
           // onScroll={() => console.log('hello')}
+          onScroll={handleonScroll}
           ref={bodyref}>
+          <button
+            onClick={scrolltobottom}
+            className={`${
+              buttonhidden ? 'hidden' : ''
+            } p-3 bg-color1 rounded-full absolute top-[65%] right-[85%] z-10`}>
+            <UilArrowDown />
+          </button>
           <MessageDateGroup date={'2023-07-20'}>
             {messages.map((message, index) => (
-              <div key={index} onContextMenu={(e) => handleRightClick(e, index)}>
+              <div key={index} 
+              // onContextMenu={(e) => handleRightClick(e, index)}
+              >
                 <Message
                   content={message.content}
                   isSeen={message.seen}
@@ -116,7 +150,9 @@ export default function ChatBody({ chattype }) {
           </MessageDateGroup>
           <MessageDateGroup date={'2023-07-22'}>
             {messages.map((message, index) => (
-              <div key={index} onContextMenu={(e) => handleRightClick(e, index)}>
+              <div key={index} 
+              // onContextMenu={(e) => handleRightClick(e, index)}
+              >
                 <Message
                   content={message.content}
                   isSeen={message.seen}
@@ -135,7 +171,6 @@ export default function ChatBody({ chattype }) {
             />
           </MessageDateGroup>
         </div>
-        <button onClick={scrolltobottom}>bottom</button>
         {footerallowed && (
           <div className=" h-16 w-[80%] vsmmobile:mb-[7rem] smmobile:mb-[7rem]">
             <ChatFooter />
@@ -145,8 +180,10 @@ export default function ChatBody({ chattype }) {
       {preview
         ? createPortal(
             <ImagePreviewer handleClose={() => setPreview(false)} />,
-            document.getElementById('app-holder')
-          )
+            document.getElementById('app-holder') 
+            // ||
+            // document.getElementById('root')
+             )
         : null}
     </div>
   );
