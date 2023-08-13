f73a219d07156a65d8b566ce6091e740f7c3803c
NimaSaei379 <saeinima@yahoo.com>
2023-08-13 13:55:02 +0330

    create profile slice

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { getTheme } from '../utility/useLoclStorage';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { getTheme } from '../utility/useLoclStorage';
 import Requests from '../API/Requests';
+import { loginUserProfile } from '../features/profileSlice';
+
 export default function Login() {
   const RequestHandeler = Requests();
+  const dispatch = useDispatch();
   const [values, setValues] = useState({
     email: '',
     password: ''
@@ -19,13 +23,10 @@ export default function Login() {
     // Check for empty fields
     if (!values.email || !values.password) {
       alert('لطفاً همه فیلدها را پر کنید.');
-      return;
     } else {
-        const res = await RequestHandeler.Login(values.email,values.password);
-        console.log(res);
-
+      dispatch(loginUserProfile(values));
     }
-  };
+  }
 
   const changeinput = (name, value) => {
     setValues({
@@ -36,18 +37,19 @@ export default function Login() {
   return (
     <div dir="rtl" className="bg-complete">
       <div className="main-div">
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col items-center justify-center">
           <a href="/">
             <img width={100} src="images/logo.png" />
           </a>
           <div className="mb-5 text-[20px] text-text1">ورود</div>
         </div>
-        <div className="w-full px-6 py-4  overflow-hidden bg-color1 shadow-md  border rounded sm:max-w-lg sm:rounded-lg ">
+        <div className="sm:max-w-lg sm:rounded-lg w-full  overflow-hidden rounded border  bg-color1 px-6 py-4 shadow-md ">
           <form>
             <div>
-                ایمیل
-              <label htmlFor="name" className="block text-sm font-medium text-text1 undefined mt-3">
-              </label>
+              ایمیل
+              <label
+                htmlFor="name"
+                className="undefined mt-3 block text-sm font-medium text-text1"></label>
               <div className="flex flex-col items-start">
                 <input
                   type="email"
@@ -60,7 +62,7 @@ export default function Login() {
               </div>
             </div>
             <div className="mt-4">
-              <label htmlFor="password" className="block text-sm font-medium text-text1 undefined">
+              <label htmlFor="password" className="undefined block text-sm font-medium text-text1">
                 رمز عبور
               </label>
               <div className="flex flex-col items-start">
@@ -77,10 +79,10 @@ export default function Login() {
             <a href="#" className="text-xs text-color4 hover:underline">
               رمز عبور خود را فراموش کرده اید؟
             </a>
-            <div className="flex items-center mt-4">
+            <div className="mt-4 flex items-center">
               <button
                 onClick={handleRegisterClick}
-                className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-[#1e7889] rounded-md hover:bg-[#7fc2cf] focus:outline-none focus:bg-[#135461]">
+                className="w-full rounded-md bg-[#1e7889] px-4 py-2 tracking-wide text-white transition-colors duration-200 hover:bg-[#7fc2cf] focus:bg-[#135461] focus:outline-none">
                 ورود
               </button>
             </div>
@@ -93,7 +95,7 @@ export default function Login() {
               </a>
             </span>
           </div>
-          <div className="flex items-center w-full my-4">
+          <div className="my-4 flex w-full items-center">
             <hr className="w-full" />
             <p className="px-3 text-text1">یا</p>
             <hr className="w-full" />
@@ -102,11 +104,11 @@ export default function Login() {
             <button
               aria-label="Login with Google"
               type="button"
-              className="flex items-center justify-center w-full p-2 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400">
+              className="flex w-full items-center justify-center space-x-4 rounded-md border p-2 focus:ring-2 focus:ring-violet-400 focus:ring-offset-1 dark:border-gray-400">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 32 32"
-                className="w-5 h-5 fill-current mx-5 text-text1">
+                className="mx-5 h-5 w-5 fill-current text-text1">
                 <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
               </svg>
               <p className="text-text1">ورود با حساب گوگل</p>
--- src/features/profileSlice.js
+++ src/features/profileSlice.js
@@ -1,18 +1,43 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import Requests from '../API/Requests';
 
-const initialState = {};
-const registerUserProfile = createAsyncThunk('profile/fetchProfileData', () => {
-  const data = Requests();
+const initialState = {
+  jwt: '',
+  profileData: {}
+};
+const registerUserProfile = createAsyncThunk('profile/sendRegisterData', async (body) => {
+  console.log(body);
+  try {
+    const data = await Requests().Register(body);
+    console.log(data.data);
+    return data.data;
+  } catch (error) {}
+});
+
+const loginUserProfile = createAsyncThunk('profile/sendLoginData', async (body) => {
+  console.log(body);
+  try {
+    const data = await Requests().Login(body);
+    console.log(data.data);
+    return data.data;
+  } catch (error) {
+    console.log(error);
+  }
 });
 const profileSlice = createSlice({
   name: 'profile',
   initialState,
 
   extraReducers: (builder) =>
-    builder.addCase(registerUserProfile.fulfilled, (state, action) => {
-      console.log(action.payload);
-    })
+    builder
+      .addCase(registerUserProfile.fulfilled, (state, action) => {
+        state.jwt = action.payload.jwt;
+        state.profileData = action.payload.profile;
+      })
+      .addCase(loginUserProfile.fulfilled, (state, action) => {
+        state.jwt = action.payload.jwt;
+        state.profileData = action.payload.profile;
+      })
 });
-
+export { registerUserProfile, loginUserProfile };
 export default profileSlice.reducer;
