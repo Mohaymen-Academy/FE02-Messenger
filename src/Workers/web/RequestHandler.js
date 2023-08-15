41edbab0128cee3eed5f7e657437179fac2c3251
hesam <bhesam2000@yahoo.com>
2023-08-15 21:44:08 +0430

    change url

--- src/Workers/web/RequestHandler.js
+++ src/Workers/web/RequestHandler.js
@@ -3,7 +3,7 @@ export default (e) => {
   // const RequestHandler = Requests();
   self.onmessage = async (msg) => {
     // console.log(msg.data);
-    fetch('http://185.60.136.202:8080/', {
+    fetch('http://185.60.136.206:8080/', {
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
