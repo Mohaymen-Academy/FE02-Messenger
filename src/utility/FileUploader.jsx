a3b5a4e651f36762c7a424c71e165d1004723b9f
NimaSaei379 <saeinima@yahoo.com>
2023-08-20 17:31:35 +0330

    create uploader file

--- src/utility/FileUploader.jsx
+++ src/utility/FileUploader.jsx
@@ -33,6 +33,7 @@ export default function FileUploader({ openpull, openfile, chattype }) {
     reader.onload = (e) => {
       const base64string = e.target.result.split(',')[1];
       base64img.current = base64string;
+      console.log(base64string);
       openfile({
         content: base64string,
         fileName: uploadedFile.current.name,
