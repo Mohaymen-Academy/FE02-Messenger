3b75550c3856b664f38e263dafc75e3d74e23a44
NimaSaei379 <saeinima@yahoo.com>
2023-08-22 10:15:25 +0330

    change the server url

--- src/Workers/web/RequestHandler.js
+++ src/Workers/web/RequestHandler.js
@@ -1,26 +1,24 @@
 // import Requests from '../../API/Requests';
 export default (e) => {
+  // const RequestHandler = Requests();
   self.onmessage = (msg) => {
-    let interval;
-    if (!msg.data.shoulddead) {
-      interval = setInterval(async () => {
-        fetch(
-          `http://185.60.136.206:8080?` +
-            new URLSearchParams({
-              active_chat: 0
-            }),
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-              Authorization: `${msg.data.token}`
-            },
-            method: 'GET'
-          }
-        )
-          .then((resp) => resp.json())
-          .then((data) => postMessage(data));
-      }, 1000);
-    }
+    // console.log(msg.data);
+    // http://185.60.136.206:8080
+    // console.log(msg.data);
+    fetch(
+      `http://192.168.70.223:8080?${new URLSearchParams({
+        active_chat: msg.data.chatID
+      })}`,
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          Authorization: `${msg.data.token}`
+        },
+        method: 'GET'
+      }
+    )
+      .then((resp) => resp.json())
+      .then((data) => postMessage(data));
   };
 };
