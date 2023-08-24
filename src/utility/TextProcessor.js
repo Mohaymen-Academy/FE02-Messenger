bb9168ecc8d61e3713244e1cce053c954e63c46a
hesam <bhesam2000@yahoo.com>
2023-08-24 10:22:54 +0430

    push in main

--- src/utility/TextProcessor.js
+++ src/utility/TextProcessor.js
@@ -96,6 +96,7 @@ export default function TextProcessorObj(containers) {
     ProcessorValues.current.caretPosition = result[0];
     // console.log(ProcessorValues.current.caretPosition);
     ProcessorValues.current.rawtext = RawText();
+    if (ProcessorValues.current.rawtext == '') ProcessorValues.current.sorted = [];
     updateEnteties(result[1]);
   }
   function handleKeyLeftRight(e) {
@@ -254,6 +255,13 @@ export default function TextProcessorObj(containers) {
     const list = generateEntity(targetref, text, ents);
     return list;
   }
+  /**
+   *
+   * @param {HTMLElement} ref
+   * @param {String} text
+   * @param {Array} ents
+   * @returns
+   */
   function generateEntity(ref, text, ents) {
     console.error(ents);
     let list_of_renderableentities = [];
@@ -291,37 +299,59 @@ export default function TextProcessorObj(containers) {
       return list_of_renderableentities;
     }
 
-    let prevEnd;
     for (let i = 0; i < ents.length; i++) {
-      if (i == 0 && ents[0].lower > 0) {
-        list_of_renderableentities.push({
-          lower: 0,
-          upper: ents[0].lower - 1
-        });
-        prevEnd = ents[0].upper;
-      }
-      if (i == 0 && ents[0].lower == 0) {
-        list_of_renderableentities.push({
-          lower: ents[0].upper + 1,
-          upper: ents.length == 1 ? text.length - 1 : ents[1].lower - 1
-        });
-        prevEnd = ents[0].upper;
-      }
+      // if (i == 0 && ents[0].lower > 0) {
+      //   list_of_renderableentities.push({
+      //     lower: 0,
+      //     upper: ents[0].lower - 1
+      //   });
+      //   prevEnd = ents[0].upper;
+      // }
+      // if (i == 0 && ents[0].lower == 0) {
+      //   list_of_renderableentities.push({
+      //     lower: ents[0].upper + 1,
+      //     upper: ents.length == 1 ? text.length - 1 : ents[1].lower - 1
+      //   });
+      //   prevEnd = ents[0].upper;
+      // }
 
-      const currentRange = ents[i];
-      const gapStart = prevEnd + 1;
-      const gapEnd = currentRange.lower - 1;
-      if (gapStart <= gapEnd) {
-        list_of_renderableentities.push({ lower: gapStart, upper: gapEnd });
+      // const currentRange = ents[i];
+      // const gapStart = prevEnd + 1;
+      // const gapEnd = currentRange.lower - 1;
+      // if (gapStart <= gapEnd) {
+      //   list_of_renderableentities.push({ lower: gapStart, upper: gapEnd });
+      // }
+      // prevEnd = Math.max(prevEnd, currentRange.upper);
+
+      // the first one
+      let lower, upper, lowertext;
+      if (i == 0) {
+        lower = 0;
+        upper = ents[i].lower - 1;
+        lowertext = text.substring(lower, upper);
+        if (lowertext != '') {
+          list_of_renderableentities.push({ lower, upper });
+        }
+        lower = ents[i].upper + 1;
+        upper = ents[i + 1]?.lower - 1 || text.length - 1;
+        list_of_renderableentities.push({ lower, upper });
       }
-      prevEnd = Math.max(prevEnd, currentRange.upper);
+      // before the last one
+      else if (i != ents.length - 1) {
+        lower = ents[i].upper + 1;
+        upper = ents[i + 1].lower - 1;
+        list_of_renderableentities.push({ lower, upper });
+      } else {
+        lower = ents[i].upper + 1;
+        upper = text.length - 1;
+        lowertext = text.substring(lower, upper);
+        if (lowertext != '') {
+          list_of_renderableentities.push({ lower, upper });
+        }
+        list_of_renderableentities.push({ lower, upper });
+      }
     }
-    if (prevEnd < text.length) {
-      list_of_renderableentities.push({
-        lower: prevEnd + 1,
-        upper: text.length - 1
-      });
-    }
+
     list_of_renderableentities = list_of_renderableentities.concat(ents);
     list_of_renderableentities.sort(customSort);
     list_of_renderableentities = list_of_renderableentities.map((ent) => ({
